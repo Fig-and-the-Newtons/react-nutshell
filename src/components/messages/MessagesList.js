2880726f3579a4a7961d71a61a8c5681484c42af
Figamus <fig74656@yahoo.com>
2018-09-06 14:53:06 -0500

    All messages append user name by ID, and tag with current date

--- src/components/messages/MessagesList.js
+++ src/components/messages/MessagesList.js
@@ -2,12 +2,21 @@ import React, { Component } from "react"
 import "./Messages.css"
 
 export default class MessagesList extends Component {
+    componentDidMount() {
+        console.log(this.props.user.id)
+        const user = this.props.user.id
+        let newState = {};
+        newState.userId = user
+        newState.message = "";
+        newState.date = Date.now()
+        this.setState(newState)
+    }
     state = {
-        userId: 1,
+        userId: {},
         message: "",
         date: Date.now()
     }
-
+    
     handleFieldChange = evt => {
         evt.preventDefault();
         const stateToChange = {}
@@ -20,7 +29,20 @@ export default class MessagesList extends Component {
         message.value = "";
     }
 
+    get = (resource, id) => {
+        this.props.get(resource, id)
+        .then((userObject) => {
+            let name = userObject.userName
+            return name
+        })
+    }
+
     render () {
+        let newMessage = {
+            userId: this.props.user.id,
+            message: this.state.message,
+            date: Date.now()
+        }
         return (
             <React.Fragment>
                 <div id="messagesContainer">
@@ -30,11 +52,13 @@ export default class MessagesList extends Component {
                 <div>
                     <section className="messages">
                     {
-                        this.props.messages.map(messageObject => 
-                            <div id={`message--${messageObject.id}`} key={messageObject.id} className="card">
+                        this.props.messages.map(messageObject => {
+                            console.log(this.props.user.id)
+                            let currentUser = this.props.users.find(u => u.id === messageObject.userId);
+                            return <div id={`message--${messageObject.id}`} key={messageObject.id} className="card">
                                 <div className="card-body">
                                     <h5 className="card-title">
-                                        {messageObject.userId}
+                                        {currentUser.userName}
                                     </h5>
                                     <p className="message-text">{messageObject.message}</p>
                                     <button onClick={() => this.props.delete("messages", messageObject.id)} className="card-link btn btn-success">Delete</button>
@@ -43,13 +67,13 @@ export default class MessagesList extends Component {
                                     }>Edit</button>
                                 </div>
                             </div>
-                        )
+                        })
                     }
                     </section>
                 </div>
                 <textarea ref="message" id="message" onChange={this.handleFieldChange}></textarea>
                 <button id="submitButton" type="button" className="btn btn-success" onClick={() => {
-                        this.props.post("messages", this.state)
+                        this.props.post("messages", newMessage)
                         .then(this.handleSubmit)
                     }}>Post Message</button>
                 </div>
